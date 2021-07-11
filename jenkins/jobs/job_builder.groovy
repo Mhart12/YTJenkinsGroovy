d3b2006b06374e82d357a6541b838d692e4f9f4e
Monica Hart <mhart@mhart-kcmo.vml.lan>
2021-07-11 15:22:50 -0500

    creating new directory

--- jenkins/jobs/job_builder.groovy
+++ jenkins/jobs/job_builder.groovy
@@ -1,3 +1,4 @@
 //root job_builder Groovy file
-import Office_365_Email_Notification
+//import jobs.Office_365_Email_Notification
+
 Office_365_Email_Notification.buildMyJob(job('Office365 Email Notification'))
