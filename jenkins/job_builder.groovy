30447a194ec662371f266a7181bf610c00fb336c
Monica Hart <mhart@mhart-kcmo.vml.lan>
2021-07-11 15:36:02 -0500

    renaming and rearranging

---
+++ jenkins/job_builder.groovy
@@ -0,0 +1,2 @@
+//root job_builder Groovy file
+Office_365_Email_Notification.buildMyJob(job('Office365_Email_Notification_Groovy'))
--- jenkins/jobs/job_builder.groovy
+++
@@ -1,2 +0,0 @@
-//root job_builder Groovy file
-Office_365_Email_Notification.buildMyJob(job('Testing'))
