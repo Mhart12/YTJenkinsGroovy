20c824524f71f01cbe7d6ba4fd62c99428d5166c
Monica Hart <mhart@mhart-kcmo.vml.lan>
2021-07-11 15:27:10 -0500

    testing

--- jenkins/jobs/job_builder.groovy
+++ jenkins/jobs/job_builder.groovy
@@ -1,4 +1,2 @@
 //root job_builder Groovy file
-//import jobs.Office_365_Email_Notification
-
-Office_365_Email_Notification.buildMyJob(job('Office365 Email Notification'))
+Office_365_Email_Notification.buildMyJob(job('Testing'))
