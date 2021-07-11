d2e4557149e21b3bdf815f40f3947e986193a859
Monica Hart <mhart@mhart-kcmo.vml.lan>
2021-07-11 17:15:55 -0500

    fine tuning email

--- jenkins/Office_365_Email_Notification.groovy
+++ jenkins/Office_365_Email_Notification.groovy
@@ -22,10 +22,10 @@ echo foobar > ${WORKSPACE}/superimportantreport.txt
                     attachBuildLog(true)
                     attachmentPatterns('**/superimportantreport.txt')
                     compressBuildLog(true)
-                    recipientList('mhart2012@gmail.com')
+                    recipientList('mhart2012@gmail.com, monica.hart@vmlyr.com')
                     replyToList('noreply@gmail.com')
-                    defaultSubject('Office365_Relay_Job_Results')
-                    defaultContent('Here is the report for the successful completion of this super important job.')
+                    defaultSubject('[$BUILD_STATUS] - Office365_Relay_Job_Results - Build #$BUILD_ID')
+                    defaultContent('<pre style=\'font-family: "TimesNewRoman"; font-size: 17px;\'>Here is the report for the successful completion of this super important job.</pre>')
                     triggers {
                         success {
                             sendTo {
